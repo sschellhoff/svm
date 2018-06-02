329981a9e81e806ed55894533e7074159a23f94f
sschellhoff <soeren.schellhoff@gmail.com>
2018-06-02 19:13:56 +0200

    implemented alloc, realloc and free functionality for the stack/heap

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,8 @@
 #include "stack.hpp"
+#include <functional>
+#include <cstring>
 
-Stack::Stack() : SP(data) {
+Stack::Stack() : SP(data), HP(SP + MAX_ADDRESS / 2) {
 }
 
 void Stack::push(int_type value) {
@@ -31,6 +33,54 @@ void Stack::reserveStackSpace(int_type size) {
 
 void Stack::removeStackSpace(int_type size) {
   SP -= size;
+}
+
+int_type Stack::reserveHeapSpace(int_type size) {
+  auto last = MAX_ADDRESS + 1; // top
+
+  for(const auto &entry : heapEntries) {
+    if(last - (entry.address + entry.size) >= size) {
+      HeapData newEntry;
+      newEntry.address = entry.address + entry.size;
+      newEntry.size = size;
+      heapEntries.push_back(newEntry);
+      std::sort(heapEntries.begin(), heapEntries.end(), [](HeapData a, HeapData b) { return a.address > b.address; });
+      return newEntry.address;
+    }
+  }
+
+  return 0;
+}
+
+void Stack::removeHeapSpace(int_type address) {
+  for(auto it = heapEntries.begin(); it != heapEntries.end(); it++) {
+    if(it->address == address) {
+      heapEntries.erase(it);
+      return;
+    }
+  }
+}
+
+int_type Stack::resizeHeapSpace(int_type address, int_type new_size) {
+  auto it = std::find_if(heapEntries.begin(), heapEntries.end(), [address](HeapData data) { return data.address == address; });
+  if(it == heapEntries.end()) {
+    return reserveHeapSpace(new_size);
+  }
+  auto size = it->size;
+  auto next = it + 1;
+  if(next != heapEntries.end()) {
+    if(next->address - it->address >= new_size) {
+      it->size = new_size;
+      return address;
+    }
+  }
+  removeHeapSpace(address);
+  auto new_address = reserveHeapSpace(new_size);
+  if(new_address == 0) {
+    return 0;
+  }
+  std::memcpy(&data[new_address], &data[address], size);
+  return new_address;
 }
 
 bool Stack::isEmpty() const {
--- stack.hpp
+++ stack.hpp
@@ -2,11 +2,19 @@
 
 #include "types.hpp"
 #include <cstddef>
+#include <vector>
 
 struct Stack {
   static constexpr std::size_t MAX_ADDRESS = 1048576; // 4mb, (word-size: 4 byte)
   int_type data[MAX_ADDRESS + 1];
   int_type* SP;
+  int_type* HP;
+  struct HeapData {
+    int_type address;
+    int_type size;
+  };
+
+  std::vector<HeapData> heapEntries;
 
   Stack();
   void push(int_type value);
@@ -19,6 +27,10 @@ struct Stack {
 
   void reserveStackSpace(int_type size);
   void removeStackSpace(int_type size);
+
+  int_type reserveHeapSpace(int_type size);
+  void removeHeapSpace(int_type address);
+  int_type resizeHeapSpace(int_type address, int_type new_size);
 
   bool isEmpty() const;
   void reset();
