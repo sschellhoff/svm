b349e522ebf1c42da174efbd4b62032b5771aa7a
sschellhoff <soeren.schellhoff@gmail.com>
2018-06-04 01:33:00 +0200

    added write bit and read line systemcalls.

--- system_calls.cpp
+++ system_calls.cpp
@@ -1,8 +1,12 @@
 #include "system_calls.hpp"
 #include "stack.hpp"
 #include <iostream>
+#include <string>
 
 SystemCalls::SystemCalls(Stack& memory) : memory(memory) {
+}
+
+SystemCalls::~SystemCalls() {
 }
 
 void SystemCalls::execute(command_type function) {
@@ -12,7 +16,14 @@ void SystemCalls::execute(command_type function) {
       std::cout << static_cast<char>(memory.data[address]);
     }
     break;
-    case 1:
+    case 1: {
+      std::string value;
+      std::getline(std::cin, value);
+      for(const auto c : value) {
+        memory.push(c);
+      }
+      memory.push(0);
+    }
     break;
     default:
       std::cerr << "Unknown systemcall: " << function << std::endl;
--- system_calls.hpp
+++ system_calls.hpp
@@ -9,5 +9,6 @@ private:
   Stack& memory;
 public:
   SystemCalls(Stack& memory);
+  ~SystemCalls();
   void execute(command_type function);
 };
